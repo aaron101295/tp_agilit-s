c7a098be679c06bab5a0b7d73357f8055dcf5830
aaron.lellouche <aaron.lellouche@etu.u-pec.fr>
2019-01-15 09:50:24 +0100

    Tests automatisés

--- src/test/java/VoitureInvocationStepDefinitionTest.java
+++ src/test/java/VoitureInvocationStepDefinitionTest.java
@@ -3,7 +3,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-
+//Test
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, tags = "@voiture", features = "src/main/resources/features", plugin = {
 "pretty", "html:target/cucumber" }, glue = "definiton")
